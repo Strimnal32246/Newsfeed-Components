983910c8fe6222fa5adadacba9a4ee1176bb053b
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-08-07 16:59:32 -0400

    added menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,7 +9,7 @@ let menuItems = [
   'Log Out'
 ];
 
-/* 
+/*
 
   Step 1: Write a function that will create a menu component as seen below:
 
@@ -21,15 +21,33 @@ let menuItems = [
 
   Pass the function an array as it's only argument.
 
-  Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
+  Step 2: Iterate over the array creating a list item <li> element for each item in the array.
   Add those items to the <ul>
 
   Step 3: Using a DOM selector, select the menu button currently on the DOM.
-  
+
   Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
 
   Step 5: return the menu component.
 
   Step 6: add the menu component to the DOM.
-  
+
 */
+const toggleMenu = () => {
+  // Toggle the "menu--open" class on your menu refence.
+  menu.classList.toggle("menu--open");
+};
+
+// Start Here: Create a reference to the ".menu" class
+const menu = document.querySelector(".menu");
+
+// create a reference to the ".menu-button" class
+const menuButton = document.querySelector(".menu-button");
+
+// Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener("click", () => {
+  toggleMenu();
+});
+
+TweenMax.from(".menu", 2, { x: -600, opacity: 0 });
+TweenMax.staggerFrom(".menu li", 1, { x: -600, opacity: 0 }, 0.2);
